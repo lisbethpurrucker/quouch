35d9aa9be89b8170bd3b59e2ec249243ff00a1be
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2023-04-20 15:41:26 +0300

    add plan and subscription model

--- db/schema.rb
+++ db/schema.rb
@@ -120,6 +120,26 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_08_154758) do
     t.index ["recipient_type", "recipient_id"], name: "index_notifications_on_recipient"
   end
 
+  create_table "payments", force: :cascade do |t|
+    t.string "checkout_session_id"
+    t.string "payment_intent"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "amount_cents", default: 0, null: false
+    t.integer "operation"
+  end
+
+  create_table "plans", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "interval"
+    t.integer "price_cents"
+    t.string "stripe_price_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "reviews", force: :cascade do |t|
     t.text "content"
     t.integer "rating"
@@ -131,6 +151,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_08_154758) do
     t.index ["booking_id"], name: "index_reviews_on_booking_id"
     t.index ["couch_id"], name: "index_reviews_on_couch_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.bigint "plan_id", null: false
+    t.bigint "user_id", null: false
+    t.boolean "active", default: true
+    t.string "stripe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["plan_id"], name: "index_subscriptions_on_plan_id"
+    t.index ["user_id"], name: "index_subscriptions_on_user_id"
   end
 
   create_table "user_characteristics", force: :cascade do |t|
@@ -195,6 +226,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_08_154758) do
   add_foreign_key "reviews", "bookings"
   add_foreign_key "reviews", "couches"
   add_foreign_key "reviews", "users"
+  add_foreign_key "subscriptions", "plans"
+  add_foreign_key "subscriptions", "users"
   add_foreign_key "user_characteristics", "characteristics"
   add_foreign_key "user_characteristics", "users"
   add_foreign_key "users", "users", column: "invited_by_id"
