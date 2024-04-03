197922fb04c8a486dc9011c663199df9ed45a27c
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2024-04-03 10:51:19 +0200

    add business model

---
+++ db/migrate/20240402200856_create_businesses.rb
@@ -0,0 +1,8 @@
+class CreateBusinesses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :businesses do |t|
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_27_150030) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_02_200856) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,6 +58,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_27_150030) do
     t.boolean "flexible", default: false
     t.index ["couch_id"], name: "index_bookings_on_couch_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "businesses", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "characteristics", force: :cascade do |t|
